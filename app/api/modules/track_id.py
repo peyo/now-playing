d38b5774eac38b9c598a18d9de4a4fbde9a6d98a
peyo <peteryyoon@gmail.com>
2023-06-14 09:26:00 -0700

    Updated spacing between buttons and the song display.

--- app/api/modules/track_id.py
+++ app/api/modules/track_id.py
@@ -12,14 +12,16 @@ async def identify_track(output_folder):
             out = await shazam.recognize_song(latest_file)
             title = out['track']['title']
             subtitle = out['track']['subtitle']
-            print("track_id.py:", title, "-", subtitle)
-            save_track_data(title, subtitle)  # Save the data to the text file
-            return title, subtitle
+            print("track_id.py:", subtitle, "-", title)
+            save_track_data(subtitle, title)  # Save the data to the text file
+            return subtitle, title
         except KeyError:
-            print("track_id.py:", "Error: No recent track information found.")
+            print("track_id.py:", "No recent track information found.")
+            save_track_data("Loading...", "_ -  zzZ")  # Save the data to the text file
             return None, None
   else:
         print("track_id.py:", "No files found in the output folder.")
+        save_track_data("Loading...", "_ -  zzZ")  # Save the data to the text file
         return None, None
 
 def get_latest_file(output_folder):
@@ -32,7 +34,7 @@ def get_latest_file(output_folder):
     return latest_file
 
 # Save track information to track_database.database.txt
-def save_track_data(title, subtitle):
+def save_track_data(subtitle, title):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     database_dir = os.path.join(current_dir, '..', '..', 'track_database')
     os.makedirs(database_dir, exist_ok=True)  # Create the 'track_database' directory if it doesn't exist
@@ -40,7 +42,7 @@ def save_track_data(title, subtitle):
     database_file = os.path.join(database_dir, 'database.txt')
 
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-    data = f"{timestamp} | {title} - {subtitle}\n"
+    data = f"{timestamp} | {subtitle} - {title}\n"
 
     with open(database_file, 'a') as file:
         file.write(data)
